6406e6a30fcf25051823c3248d26e08aa612e09d
Feddis08 <feddis08@gmail.com>
2025-11-13 14:40:42 +0100

    fixed portal screen. now it's always showing. overriding Mainmenu/Serverselection/...

--- src/main/java/at/RIEMER/client/guis/PortalScreenOpener.java
+++ src/main/java/at/RIEMER/client/guis/PortalScreenOpener.java
@@ -2,7 +2,11 @@ package at.RIEMER.client.guis;
 
 import at.RIEMER.client.guis.ArkPortalScreen;
 import at.RIEMER.core.Main;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.client.gui.screen.MultiplayerScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.WorldSelectionScreen;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,15 +15,24 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public final class PortalScreenOpener {
 
-    private static boolean opened = false;
 
     @SubscribeEvent
     public static void onGuiOpen(GuiOpenEvent e) {
-        if (opened) return;
-        if (!(e.getGui() instanceof MainMenuScreen)) return;
+        Screen gui = e.getGui();
 
-        opened = true;
-        e.setGui(new ArkPortalScreen());
+        if (gui instanceof ArkPortalScreen) return;
+
+
+        if (gui instanceof MainMenuScreen
+                || gui instanceof MultiplayerScreen
+                || gui instanceof WorldSelectionScreen) {
+
+            // override: Portal statt original Screen
+            e.setGui(new ArkPortalScreen());
+        }
+
+
+
     }
 
     private PortalScreenOpener() {}
